342e3c10be022288aab41661a46f756f88af2288
ME (Mac mini) <rxseven.com@gmail.com>
2019-03-11 14:53:45 +0700

    Add route handler for fetching images

--- src/routes/gallery.ts
+++ src/routes/gallery.ts
@@ -1,6 +1,11 @@
 import Router from 'express-promise-router';
 
+import galleryController from '../controllers/gallery';
+
 // Creates a new router object
 const router = Router();
+
+// Route handlers
+router.route('/').get(galleryController.getImages);
 
 export default router;
