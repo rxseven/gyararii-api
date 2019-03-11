5010aaa7b8b767372729cdb5656446e8e1f2d9ef
ME (Mac mini) <rxseven.com@gmail.com>
2019-03-11 13:52:38 +0700

    Specify client origin

--- src/config/server.ts
+++ src/config/server.ts
@@ -4,4 +4,10 @@ const API_VERSION = 1;
 // Port
 const PORT = process.env.PORT || 5000;
 
-export { API_VERSION, PORT };
+// Client origin
+const CLIENT_ORIGIN =
+  process.env.NODE_ENV === 'production'
+    ? 'https://gyararii.herokuapp.com'
+    : `http://localhost:3000`;
+
+export { API_VERSION, CLIENT_ORIGIN, PORT };
