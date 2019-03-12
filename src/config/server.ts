85418b1aec89446dc61a23a19eb7d84084baed20
ME (Mac mini) <rxseven.com@gmail.com>
2019-03-12 14:11:31 +0700

    Add additional client origins for development environment

--- src/config/server.ts
+++ src/config/server.ts
@@ -6,7 +6,16 @@ const PORT = process.env.PORT || 5000;
 
 // Client origin
 const whitelist = {
-  development: ['http://localhost:3000'],
+  development: [
+    'http://localhost:3000',
+    'http://localhost:6006',
+    'http://localhost:8080',
+    'http://localhost:8081',
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:6006',
+    'http://127.0.0.1:8080',
+    'http://127.0.0.1:8081'
+  ],
   production: ['https://gyararii.herokuapp.com']
 };
 
